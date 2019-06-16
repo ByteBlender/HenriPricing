119d12f6160f6831699e6b0107d1d6498f5d1d95
GAYAN-PROBOOK\Gayan <thebyteblender@gmail.com>
2019-06-16 20:16:36 +0800

    init

--- HenriPriceCalculator/Controllers/HomeController.cs
+++ HenriPriceCalculator/Controllers/HomeController.cs
@@ -3,15 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HenriPriceCalculator.Models;
 
 namespace HenriPriceCalculator.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string txtP1, string txtM3, string BrandName)
         {
-            return View();
+            ProductModel p = new ProductModel();
+            double m3 = 0;
+            double p1 = 0;
+            if(!string.IsNullOrWhiteSpace(txtP1) || !string.IsNullOrWhiteSpace(txtP1))
+            {
+                p.P1 = txtP1;
+                p.M3 = txtM3;
+                p1 = double.TryParse(txtP1, out p1)? p1 : 0;
+                m3= double.TryParse(txtM3, out  m3) ? m3 : 0;
+                double shipping = CL_HenriPricing.Pricing.CalcShipping(m3, BrandName);
+                p.Shipping = shipping.ToString();
+                p.RRP = CL_HenriPricing.Pricing.CalcRRP(BrandName, p1, shipping).ToString();
+
+            }
+         
+            return View(p);
         }
+
 
         public ActionResult About()
         {
---
+++ HenriPriceCalculator/Models/ProductModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CL_HenriPricing;
+
+namespace HenriPriceCalculator.Models
+{
+    public class ProductModel
+    {
+        public string P1 { get; set; }
+        public string M3 { get; set; }
+        public string Shipping { get; set; }
+        public string RRP { get; set; }
+        public string BrandName { get; set; }
+        public List<string> Brands { get; set; }
+
+
+
+        public ProductModel()
+        {
+            List<string> brands = new List<string>();
+            foreach (var item in Brand.CreateBrandList())
+            {
+                brands.Add(item.Name);
+            }
+            Brands = brands;
+        }
+
+    }
+}
